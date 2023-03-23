32d4d1dc5ea2bead677e47ff2e82aaf192f0cb0a
Jashua <77981430+81Jashua@users.noreply.github.com>
2023-03-23 14:25:54 -0600

    added route for stories by user

--- routes/stories.js
+++ routes/stories.js
@@ -58,6 +58,25 @@ router.get('/:id', ensureAuth, async (req, res) => {
   }
 })
 
+// Show all stories by User
+router.get('/user/:userId', ensureAuth, async (req, res) => {
+  try {
+    const stories = await Story.find({
+      user: req.params.userId,
+      status: 'public',
+    })
+      .populate('user')
+      .lean()
+
+    res.render('stories/index', {
+      stories,
+    })
+  } catch (err) {
+    console.error(err)
+    res.render('error/500')
+  }
+})
+
 
 // Show edit story page
 router.get('/edit/:id', ensureAuth, async (req, res) => {
